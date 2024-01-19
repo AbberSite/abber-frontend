56a877e9e25a30e11df4b0bc6cf8338f56fadfd6
hdood <m.bouguerzi1234@gmail.com>
2024-01-19 12:04:07 +0100

    fixing blog post width and adding skeletons

---
+++ components/shared/skeletons/BlogPost.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="animate-pulse lg:max-w-full w-full">
+        <div class="flex items-center justify-between">
+            <div class="h-2 w-[35%] rounded-full bg-gray-200"></div>
+            <div class="h-2 w-[5%] rounded-full bg-gray-200"></div>
+        </div>
+        <div class="mb-10 w-full space-y-4 pt-10">
+            <div class="h-2 rounded-full bg-gray-200"></div>
+            <div class="h-2 w-[35%] rounded-full bg-gray-200"></div>
+        </div>
+        <div class="aspect-[3/2] w-full rounded-xl bg-gray-200"></div>
+        <div class="w-full space-y-5 pt-10">
+            <div class="w-full space-y-4">
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 w-[35%] rounded-full bg-gray-200"></div>
+            </div>
+            <div class="w-full space-y-4">
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 w-[35%] rounded-full bg-gray-200"></div>
+            </div>
+            <div class="w-full space-y-4">
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 w-[35%] rounded-full bg-gray-200"></div>
+            </div>
+            <div class="w-full space-y-4">
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 rounded-full bg-gray-200"></div>
+                <div class="h-2 w-[35%] rounded-full bg-gray-200"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ components/shared/skeletons/Toc.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="animate-pulse space-y-4 px-6">
+        <p class="h-2 rounded-full bg-gray-200"></p>
+        <p class="h-2 rounded-full bg-gray-200"></p>
+        <p class="h-2 rounded-full bg-gray-200"></p>
+        <p class="h-2 rounded-full bg-gray-200"></p>
+        <p class="h-2 rounded-full bg-gray-200"></p>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
