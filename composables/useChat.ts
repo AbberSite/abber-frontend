0cd373de7c6d15552ac7a8ab670d8b77a9fca20e
tahaghazy <68954755+tahaghazy@users.noreply.github.com>
2024-02-20 21:28:14 +0200

    Use JWT token with WS

--- composables/useChat.ts
+++ composables/useChat.ts
@@ -1,13 +1,18 @@
 export default (orderId: string) =>
-    new Promise((resolve) => {
-        
-        // VITE_WS_URL = wss://test.abber.co
-        const exampleSocket = new WebSocket(import.meta.env.VITE_WS_URL + `/ws/order/${orderId}/`);
+  new Promise((resolve) => {
+    const { rawToken } = useAuthState();
 
-        exampleSocket.onopen = (event) => {
-            exampleSocket.send("Here's some text that the server is urgently awaiting!");
+    // VITE_WS_URL = wss://test.abber.co
+    const exampleSocket = new WebSocket(
+      import.meta.env.VITE_WS_URL +
+      `/ws/order/${orderId}/`
+      + `?authorization=JWT ${rawToken.value}`,
+    );
 
-            resolve(exampleSocket)
-          };
+    exampleSocket.onopen = (event) => {
+      exampleSocket.send("Here's some text that the server is urgently awaiting!");
 
-    });
+      resolve(exampleSocket)
+    };
+
+  });
