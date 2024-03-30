f72baab7ebc89f7eab90786ef18b8f1d9fccb153
Taha Ghazi <tahaghazt164@gmail.com>
2024-03-30 02:26:03 +0200

    Refactor environment variable in ecosystem.config.cjs and nuxt.config.ts

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,9 +6,6 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs',
-      env: {
-        NODE_ENV: "dev", 
-      }
     }
   ]
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-const production = false;
+let production = process.env.NODE_ENV === "production";
 const apiSecret = production ? "a011ff6611fa1cfa9be83e5e22533976b2ede3df" : "d378b42b1f3f18f231edb2f253e43025dc01406f";
 const websiteBasePath = production ? "https://abber.co" : "https://test.abber.co";
 const apiBasePath = websiteBasePath + "/api";
