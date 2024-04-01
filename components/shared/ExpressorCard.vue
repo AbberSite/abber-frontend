71872bf2325117da45e9f31a0af51ceb2650c370
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-04-01 11:58:16 +0000

    update the rates

--- components/shared/ExpressorCard.vue
+++ components/shared/ExpressorCard.vue
@@ -12,10 +12,24 @@
                 <div class="pt-6 font-semibold">
                     {{ expressor.seller.first_name }}
                 </div>
-                <div class="flex items-center justify-center pt-2 text-yellow-400">
+                <div class="flex items-center justify-center pt-2 ">
                     <svg
-                        v-for="i in floor(expressor.rate) ?? 1"
-                        class="flex-shrink-0"
+                        v-for="i in Math.floor(expressor.rate) ?? 1"
+                        class="flex-shrink-0 text-yellow-400"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                        height="18"
+                        width="18">
+                        <path
+                            fill-rule="evenodd"
+                            d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                    <svg
+                        v-for="i in  Math.ceil(5 - expressor.rate)"
+                        class="flex-shrink-0 text-gray-300"
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 20 20"
                         fill="currentColor"
