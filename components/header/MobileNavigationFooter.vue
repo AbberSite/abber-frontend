9405900b397c9ca1aab0fb4be5ce0ca43a966988
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-04-01 12:22:49 +0000

    added right routes for mobile navigator

--- components/header/MobileNavigationFooter.vue
+++ components/header/MobileNavigationFooter.vue
@@ -2,7 +2,7 @@
     <div class="space-y-3 py-6">
         <NuxtLink
             class="-mx-3 block rounded-lg px-3 pb-2.5 pt-4 font-semibold hover:bg-gray-50"
-            @click="$emit('navigate', { name: 'index' })"
+            @click="$emit('navigate', { name: 'about' })"
             >من نحن</NuxtLink
         >
         <NuxtLink
@@ -12,12 +12,12 @@
         >
         <NuxtLink
             class="-mx-3 block rounded-lg px-3 pb-2.5 pt-4 font-semibold hover:bg-gray-50"
-            @click="$emit('navigate', { name: 'index' })"
+            @click="$emit('navigate', { name: 'terms' })"
             >الشروط و الأحكام</NuxtLink
         >
         <NuxtLink
             class="-mx-3 block rounded-lg px-3 pb-2.5 pt-4 font-semibold hover:bg-gray-50"
-            @click="$emit('navigate', { name: 'index' })"
+            @click="$emit('navigate', { name: 'user-agreement' })"
             >إتفاقية المستخدم</NuxtLink
         >
     </div>
