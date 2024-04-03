56ce2544d9bd941b82fb0bde2b4d1af0cc473fa6
Abdullah Alnuaimi <96056894+alnuaimi94@users.noreply.github.com>
2024-04-03 23:10:59 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -111,7 +111,7 @@ export default defineNuxtConfig({
         {
           src: '/lazysizes.min.js',
           type: 'text/javascript',
-          async: true 
+          defer: true 
         }
       ],
     },
