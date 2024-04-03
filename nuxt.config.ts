06a3a4474e99fdf4629668275a723517074521a1
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-04-03 17:48:14 +0000

    fix an error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,7 +36,7 @@ export default defineNuxtConfig({
     apiSecret: apiSecret,
     apiBasePath: apiBasePath,
     public: {
-      ws_url: production ? import.meta.env.VITE_WS_URL : 'ws://localhost:3000',
+      ws_url: process.env.NODE_ENV === 'production' ? import.meta.env.VITE_WS_URL : 'ws://localhost:3000',
       zoomSdkKey: "jFmC2HUOQl6JVb_PHPXxNQ",
       websiteBasePath: websiteBasePath,
       paymentWidgetURL:`https://${!production?'test.':''}oppwa.com/v1/paymentWidgets.js`
