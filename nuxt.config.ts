4972e3faf8de43abd5baad5e11934ee4d1c6404e
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-04-03 23:43:09 +0000

    add lazysizes in body tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -111,7 +111,8 @@ export default defineNuxtConfig({
         {
           src: '/lazysizes.min.js',
           type: 'text/javascript',
-          async: true 
+          async: true,
+          body: true
         }
       ],
     },
