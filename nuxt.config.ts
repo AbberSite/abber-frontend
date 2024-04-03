f15fc28036eb57fd8beae57f26767804054a73cf
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-04-03 23:49:37 +0000

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -111,7 +111,8 @@ export default defineNuxtConfig({
         {
           src: '/lazysizes.min.js',
           type: 'text/javascript',
-          async: true 
+          defer: true, 
+          body: true
         }
       ],
     },
