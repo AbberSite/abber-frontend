08d406a9cb428427d2bbdd56443b3ac7292d030f
tahaghazy <68954755+tahaghazy@users.noreply.github.com>
2024-04-04 02:39:48 +0200

    Refactor ContactType component to use SkeletonsContactType

---
+++ components/shared/skeletons/ContactType.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="animate-pulse">
+    <div class="rounded-md border px-4 py-4 shadow-sm">
+      <div class="flex">
+        <div class="ms-3 w-full pt-2">
+          <div class="flex w-full flex-col space-y-4">
+            <div class="h-2 w-[35%] rounded-full bg-gray-200"></div>
+          </div>
+          <div class="space-y-4 pt-6">
+            <p class="h-2 rounded-full bg-gray-200"></p>
+            <p class="h-2 rounded-full bg-gray-200"></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
