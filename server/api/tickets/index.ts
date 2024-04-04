eb1be887b523880be8d9dd2d83044d0e997980ee
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-04-04 16:17:19 +0000

    connect api for support

---
+++ server/api/tickets/index.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+export default defineEventHandler(async (event) => {
+    let params = getQuery(event);
+    const headers = getHeaders(event);
+    let filters;
+    const previousParams = await useStorage().getItem('filters');
+    if(headers['x-requested-with'] != "XMLHttpRequest" && previousParams){
+        filters = Object.assign(previousParams, params);
+    } else {
+        filters = params
+    };
+    const Authorization = headers.authorization;
+    const config = useRuntimeConfig();
+    try {
+        const response = await axios.get(config.apiBasePath + '/support/problems', {
+            headers: {
+                'api-key': config.apiSecret,
+                Authorization
+            },
+            params: filters
+        });
+        await useStorage().setItem("filters", params);
+        return response.data;
+    } catch (error: any){
+        setResponseStatus(event, 400);
+        return {
+            data: error?.response?.data,
+            status: 'error',
+            error: 'حدث خطأ غير متوقع'
+        }
+    }
+})
