667ac176e03bfabc5679cff39838a2fc26523982
Taha Ghazi <tahaghazt164@gmail.com>
2024-04-16 05:42:54 +0200

    Refactor mobile navigation and fix navigation links

--- components/header/Navigation.vue
+++ components/header/Navigation.vue
@@ -2,12 +2,8 @@
     <div class="hidden md:flex md:gap-x-12 2xl:text-lg">
         <NuxtLink class="font-semibold" :to="{ name: 'index' }"> الرئيسية </NuxtLink>
         <NuxtLink class="font-semibold" :to="{ name: 'blog' }"> المدونة </NuxtLink>
-<<<<<<< HEAD
-        <NuxtLink class="font-semibold" :to="{ name: 'support' }"> مركز الدعم </NuxtLink>
-=======
         <NuxtLink class="font-semibold" v-if="status == 'authenticated'" :to="{ name: 'orders' }"> الطلبات </NuxtLink>
-        <NuxtLink class="font-semibold" :to="{ name: 'tickets' }"> مركز الدعم </NuxtLink>
->>>>>>> 5b5531bdd5ca3585349597e99dea09075d825b97
+        <NuxtLink class="font-semibold" :to="{ name: 'support' }"> مركز الدعم </NuxtLink>
     </div>
 </template>
 
