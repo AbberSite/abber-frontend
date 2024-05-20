a940b276980b3b812b82d7501aedf6b72fbeb7e5
tahaghazy <68954755+tahaghazy@users.noreply.github.com>
2024-05-20 22:52:07 +0300

    Update websiteBasePath in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 let production = process.env.ABBER_ENV === "production";
 const apiSecret = production ? "a011ff6611fa1cfa9be83e5e22533976b2ede3df" : "d378b42b1f3f18f231edb2f253e43025dc01406f";
-const websiteBasePath = production ? "https://abber.co" : "https://test.abber.co";
+const websiteBasePath = production ? "https://theme.abber.co" : "https://test.abber.co";
 const apiBasePath = websiteBasePath + "/api";
 export default defineNuxtConfig({
   devtools: {
