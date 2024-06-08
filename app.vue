f1c599dd489c0d28fc68e55c419a74d41aa12372
tahaghazy <68954755+tahaghazy@users.noreply.github.com>
2024-06-08 14:53:10 +0300

    refactor: Remove commented out code for Google ads in app.vue

--- app.vue
+++ app.vue
@@ -43,11 +43,11 @@ useHead({
       src: 'https://www.googleoptimize.com/optimize.js?id=OPT-ML3SB22',
       async: true
     },
-    {
-      src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2434981739474696',
-      async: true,
-      crossorigin: 'anonymous'
-    },
+    // {
+    //   src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2434981739474696',
+    //   async: true,
+    //   crossorigin: 'anonymous'
+    // },
     {
       innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5F7JTJM');`,
       type: 'text/javascript'
