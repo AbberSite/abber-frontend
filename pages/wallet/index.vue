f845e9bbf4d94e46664457311274ec2b236781e7
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-06-09 13:16:09 +0000

    auto scroll to the tab

--- pages/wallet/index.vue
+++ pages/wallet/index.vue
@@ -1,4 +1,5 @@
 <template>
+
     <Head>
         <title>عبر - المحفظة</title>
     </Head>
@@ -7,22 +8,14 @@
 
         <section
             class="relative mx-auto flex w-full max-w-7xl flex-col items-center px-4 pb-36 pt-28 xs:px-6 md:pt-32 lg:px-8 xl:pb-44"
-            aria-labelledby="wallet-heading"
-            x-data="{ openFilterDropdown: false, activeTab: 1 }">
+            aria-labelledby="wallet-heading" x-data="{ openFilterDropdown: false, activeTab: 1 }">
             <div class="rounded-md border border-gray-300 px-3 py-3 shadow-sm">
                 <!-- Heroicon name: outline/wallet -->
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    height="24"
-                    width="24">
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3"></path>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" height="24" width="24">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3">
+                    </path>
                 </svg>
             </div>
             <h1 class="pt-8 text-lg font-semibold xs:text-xl 2xl:text-2xl" id="wallet-heading">المحفظة</h1>
@@ -30,11 +23,11 @@
 
             <WalletTabs v-model="activeTab" />
 
-            <WalletFinancialSummary v-show="activeTab === 'summary'"  />
+            <WalletFinancialSummary v-show="activeTab === 'summary'" />
             <WalletTransactions v-if="activeTab == 'operations'" />
             <WalletStatements v-if="activeTab == 'statement'" />
             <WalletCards v-if="activeTab == 'cards'" />
-            <WalletRequests v-if="activeTab == 'withdrawalRequests'"/>
+            <WalletRequests v-if="activeTab == 'withdrawalRequests'" />
         </section>
     </main>
 </template>
@@ -50,12 +43,9 @@ const route = useRoute();
 
 // await getSession()
 
-const activeTab = ref<'summary' | 'operations' | 'statement' | 'cards' | 'withdrawalRequests'>(['summary' , 'operations' , 'statement' , 'cards' , 'withdrawalRequests'].includes(route.query?.activeRoute) ? route.query.activeRoute : 'summary');
+const activeTab = ref<'summary' | 'operations' | 'statement' | 'cards' | 'withdrawalRequests'>(['summary', 'operations', 'statement', 'cards', 'withdrawalRequests'].includes(route.query?.activeRoute) ? route.query.activeRoute : 'summary');
 
-if(!process.client){
-    // await fetchAll();
 
-}
 
 // onMounted(async () => {
 //     if (transactions.value.length === 0) {
@@ -64,6 +54,14 @@ if(!process.client){
 //         loading.value = false
 //     }
 // });
+onMounted(async () => {
+    if (['summary', 'operations', 'statement', 'cards', 'withdrawalRequests'].includes(route.query?.activeRoute)) {
+        setTimeout(() => {
+            let tab = document.getElementById((route.query?.activeRoute).toString());
+            tab?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }, 3000)
+    }
+})
 </script>
 
 <style scoped></style>
