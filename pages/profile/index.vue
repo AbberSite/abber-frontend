3641a33fd6ba661fe798d1b2d1821e3873d647d5
tahaghazy <68954755+tahaghazy@users.noreply.github.com>
2024-06-11 07:51:34 +0300

    Set active tab

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,84 +1,88 @@
 <template>
+  <Head>
+    <title v-if="!edit">عبر - الملف الشخصي</title>
+    <title v-else>عبر - {{ data.first_name }}</title>
+  </Head>
+  <Header />
+  <main class="min-h-screen outline-none">
+    <!-- Hero section -->
+    <HeroBackground />
+    <!-- Profile section -->
+    <section class="relative mx-auto flex w-full max-w-7xl flex-col items-center px-4 pb-36 pt-28 xs:px-6 sm:items-stretch md:pt-32 lg:px-8 xl:pb-44" aria-labelledby="profile-heading">
+      <ProfileHeader v-model="edit" />
 
-    <Head>
-        <title v-if="!edit">عبر - الملف الشخصي</title>
-        <title v-else>عبر - {{ data.first_name }}</title>
-    </Head>
-    <Header />
-    <main class="min-h-screen outline-none">
-        <!-- Hero section -->
-        <HeroBackground />
-        <!-- Profile section -->
-        <section
-            class="relative mx-auto flex w-full max-w-7xl flex-col items-center px-4 pb-36 pt-28 xs:px-6 sm:items-stretch md:pt-32 lg:px-8 xl:pb-44"
-            aria-labelledby="profile-heading">
-            <ProfileHeader v-model="edit" />
+      <template v-if="edit">
+        <ClientOnly>
+          <ProfileEdit />
+        </ClientOnly>
+      </template>
 
-            <template v-if="edit">
-                <ClientOnly>
-                    <ProfileEdit />
-                </ClientOnly>
+      <template v-else>
+        <ProfileTabs v-model="activeTab" />
+        <ProfileDetails v-if="activeTab == 'details'" />
+
+        <template v-if="activeTab == 'posts'">
+          <Suspense>
+            <template #fallback>
+              <div class="grid gap-x-8 gap-y-20 pt-16 sm:grid-cols-2 lg:grid-cols-3 w-full">
+                <SkeletonsPostCard />
+                <SkeletonsPostCard />
+                <SkeletonsPostCard />
+                <SkeletonsPostCard />
+                <SkeletonsPostCard />
+                <SkeletonsPostCard />
+              </div>
             </template>
 
-            <template v-else>
-                <ProfileTabs v-model="activeTab" />
-                <ProfileDetails v-if="activeTab == 'details'" />
-
-                <template v-if="activeTab == 'posts'">
-                    <Suspense>
-                        <template #fallback>
-                            <div class="grid gap-x-8 gap-y-20 pt-16 sm:grid-cols-2 lg:grid-cols-3 w-full">
-                                <SkeletonsPostCard />
-                                <SkeletonsPostCard />
-                                <SkeletonsPostCard />
-                                <SkeletonsPostCard />
-                                <SkeletonsPostCard />
-                                <SkeletonsPostCard />
-                            </div>
-                        </template>
-
-                        <template #default>
-                            <ProfileBookmarkedPosts class="w-full" />
-                        </template>
-                    </Suspense>
-                </template>
-                <template v-if="activeTab == 'informationsService'">
-                    <ProfileServiceInformation />
-                </template>
-                <template v-if="activeTab == 'subscriptions'">
-                    <PackagesSubscriptions/>
-                </template>
-
+            <template #default>
+              <ProfileBookmarkedPosts class="w-full" />
             </template>
-        </section>
-    </main>
-    <Footer v-if="!edit" />
+          </Suspense>
+        </template>
+        <template v-if="activeTab == 'informationsService'">
+          <ProfileServiceInformation />
+        </template>
+        <template v-if="activeTab == 'subscriptions'">
+          <PackagesSubscriptions />
+        </template>
+      </template>
+    </section>
+  </main>
+  <Footer v-if="!edit" />
 </template>
 
 <script setup lang="ts">
 const { fetchBookmarked } = usePostsStore();
 const edit = ref(false);
-const { data } = useAuth()
+const { data } = useAuth();
 const route = useRoute();
 
-const activeTab = ref('details');
+const activeTab = ref("details");
 onBeforeMount(() => {
-    if(route.fullPath == '/profile?status=new'){
-            if(data.value.user_type == 'معبر'){
-                navigateTo({name: 'orders'});
-            } else {
-                navigateTo('/');
-            }
+  if (route.fullPath == "/profile?status=new") {
+    if (data.value.user_type == "معبر") {
+      navigateTo({ name: "orders" });
+    } else {
+      navigateTo("/");
     }
-})
-onMounted(async () => {
-    await fetchBookmarked()
+  }
+});
 
+onMounted(async () => {
+  await fetchBookmarked();
+  activeTab.value = getActiveTab.value;
+});
+
+const getActiveTab = computed(() => {
+  if (process.client) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get("tab") || "details"; // Fallback to 'default' if 'tab' is not present
+  }
 });
 
 definePageMeta({
-    middleware: 'auth',
-    layout: false
+  middleware: "auth",
+  layout: false,
 });
 </script>
 
