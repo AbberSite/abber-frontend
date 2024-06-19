f56de4ac5c957c3cdb873e87ff213e72bbb4ba38
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-06-19 14:19:59 +0000

    update

--- pages/index.vue
+++ pages/index.vue
@@ -17,38 +17,12 @@
 </template>
 
 <script setup lang="ts">
-import axios from "axios";
-
 definePageMeta({
   middleware: ["verified"],
   auth: false,
 });
 const { data } = useAuth();
-function getAllCookies() {
-  const cookies = document.cookie.split("; ");
-  const cookieObj = {};
 
-  cookies.forEach((cookie) => {
-    const [name, value] = cookie.split("=");
-    cookieObj[name] = value;
-  });
-
-  return cookieObj;
-}
-
-try {
-  const cookies = getAllCookies();
-  const headers = {
-    "api-key": "cggh",
-  };
-  const response = await useFetch("/api/auth/session/", {
-    method: "GET",
-    headers: headers,
-  });
-  console.log(response);
-} catch (error) {
-  console.error("Error fetching data:", error);
-}
 </script>
 
 <style scoped></style>
