d5ff55c85659b39de365d86a699db7120e1a2752
tahaghazy <68954755+tahaghazy@users.noreply.github.com>
2024-06-22 19:08:40 +0300

    refactor: Remove redundant ABBER_ENV variable in cd.yml and ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,6 +6,19 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs',
-    }
-  ]
+      env: {
+        ABBER_ENV: 'production',
+      },
+    },
+    {
+      name: 'AbebrTest',
+      port: '3001',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs',
+      env: {
+        ABBER_ENV: 'dev',
+      },
+    },
+  ],
 }
