f7c9e9c3319ca7a28e3567694dbc5484d7e0d467
tahaghazy <68954755+tahaghazy@users.noreply.github.com>
2024-07-03 05:27:40 +0300

    use seearch component

--- pages/dashboardv2/orders/index.vue
+++ pages/dashboardv2/orders/index.vue
@@ -9,33 +9,7 @@
             </div>
             <div class="w-full pt-6">
                 <div class="flex items-center justify-between">
-                    <form class="w-full sm:max-w-sm" method="GET">
-                        <div class="w-full space-y-3">
-                            <div class="relative">
-                                <span
-                                    class="absolute h-[50px] items-center justify-center px-4 py-4 ltr:left-0 rtl:right-0 rtl:scale-x-flip">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor" height="20" width="20">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z">
-                                        </path>
-                                    </svg></span>
-
-                                <input v-model="filters.search" class="form-control h-[50px] px-12" type="search"
-                                    name="q" id="search" placeholder="إبحث عن طلب معين" required />
-                                <button
-                                    class="absolute h-[50px] items-center justify-center px-4 py-4 ltr:right-0 rtl:left-0 sm:hidden"
-                                    type="button" @click="openFiltersMobileModal = true">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor" height="20" width="20">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75">
-                                        </path>
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    <DashboardInputsSearch placeholder="ابحث عن طلب معين" v-model="filters.search" @openFiltersMobileModal="openFiltersMobileModal=true"/>
                     <div class="relative">
                         <button
                             class="hidden h-[50px] items-center justify-center rounded-md border bg-white px-4 py-3 text-xs font-semibold shadow-sm hover:bg-gray-50 sm:flex"
@@ -65,6 +39,7 @@
             </div>
             <SkeletonsOrdersTable v-if="loading" />
             <DashboardOrdersTable v-else :orders="orders ?? []" />
+            
             <Pagination class="pt-4" :results="(pagination as PaginationResponse<any>)" @change="fetchAll"
                 per-page="20" />
 
@@ -85,6 +60,7 @@ const openFiltersDropdown = ref(false);
 const { orders, pagination, loading, filters, filtersCount } = storeToRefs(useDashOrdersStore());
 const { fetchAll } = useDashOrdersStore();
 // const pagination = ref<PaginationResponse<any>>();
+    
 onMounted(async () => {
     fetchAll();
 });
