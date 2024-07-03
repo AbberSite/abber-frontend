d4ac611dd022d0948fdeede4eef249dfec77e752
tahaghazy <68954755+tahaghazy@users.noreply.github.com>
2024-07-03 06:14:25 +0300

    Add cell options

--- components/dashboard/tables/Row.vue
+++ components/dashboard/tables/Row.vue
@@ -1,19 +1,17 @@
 <template>
-  <td class="whitespace-nowrap pb-4 pe-12 pt-6 font-medium">
+  <td class="whitespace-nowrap pb-4 pe-12 pt-6 font-medium font-medium text-black dark:text-white">
     <slot>
-      <h5 class="font-medium text-black dark:text-white" >
-       <span v-if="name.includes('date')">{{ useArabicFormattedDate(content)}}</span>
-       <span v-else-if="content?.first_name">
-        {{ content.first_name }}
-       </span>
+        <span v-if="name.includes('date')">{{ useArabicFormattedDate(content) }}</span>
+        <span v-else-if="content?.first_name">{{ content.first_name }}</span>
+        <span v-else-if="typeof content === 'boolean'"><DashboardTablesCellsCheck :check="content"/></span>
+        <span v-else-if="name.includes('method')"><DashboardTablesCellsPaymentMethod :method="content" /></span>
         <span v-else>{{ content }}</span>
-      </h5>
     </slot>
   </td>
 </template>
 
 <script lang="ts" setup>
-defineProps<{ content: any ;name:string}>();
+defineProps<{ content: any; name: string }>();
 </script>
 
 <style></style>
---
+++ components/dashboard/tables/cells/Check.vue
@@ -0,0 +1,21 @@
+<template>
+<svg v-if="check" class="w-5 h-5 text-green-800" aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+              <path fill-rule="evenodd"
+                d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm13.707-1.293a1 1 0 0 0-1.414-1.414L11 12.586l-1.793-1.793a1 1 0 0 0-1.414 1.414l2.5 2.5a1 1 0 0 0 1.414 0l4-4Z"
+                clip-rule="evenodd" />
+            </svg>
+            <svg v-else class="w-5 h-5 text-red-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+              height="24" fill="none" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                d="m6 6 12 12m3-6a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+</template>
+
+<script lang="ts" setup>
+defineProps<{ check: any }>();
+</script>
+
+<style>
+
+</style>
