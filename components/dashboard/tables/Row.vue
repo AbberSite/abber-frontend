f7f822353d64dc0c349082ea9c3c9ee92bcb3193
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-07-08 15:46:02 +0000

    make first name is short

--- components/dashboard/tables/Row.vue
+++ components/dashboard/tables/Row.vue
@@ -2,7 +2,7 @@
   <td class="whitespace-nowrap pb-4 pe-12 pt-6 font-medium text-black dark:text-white">
     <slot>
         <span v-if="name.includes('date')">{{ useArabicFormattedDate(content) }}</span>
-        <span v-else-if="content?.first_name">{{ content.first_name }}</span>
+        <span v-else-if="content?.first_name">{{ (content.first_name as string).split(' ')[0] }}</span>
         <span v-else-if="typeof content === 'boolean'"><DashboardTablesCellsCheck :check="content"/></span>
         <span v-else-if="name.includes('method')"><DashboardTablesCellsPaymentMethod :method="content" /></span>
         <span v-else-if="name === 'status'"><template v-if="(content as string).length == 1">{{ $t(`${content}`) }}</template><OrderStatus :status="content" /></span>
