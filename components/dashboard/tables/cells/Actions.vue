10541dd8ba89415840bd9e971e3488f2ffe4e1cc
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-07-13 16:31:51 +0000

    fixed some issues in WithdrawalRequests

--- components/dashboard/tables/cells/Actions.vue
+++ components/dashboard/tables/cells/Actions.vue
@@ -2,10 +2,8 @@
   <span class="flex justify-center w-full space-x-3 space-x-reverse">
     <NuxtLink v-if="actions?.view" :to="actions.view.path + data?.id"><EyeIcon class="w-5 h-5" /></NuxtLink>
     <NuxtLink class="text-gray-900" v-if="actions?.details" :to="$route.path + data?.id"><Cog6ToothIcon class="w-5 h-5" /></NuxtLink>
-    <button class="text-green-600 hover:text-green-700" v-if="actions?.modify && data?.status == '1'" @click="modify(data?.id)"><PencilSquareIcon class="w-5 h-5" /></button>
+    <button class="text-green-600 hover:text-green-700" v-if="actions?.modify && !data?.invoice && data?.status != '3'" @click="modify(data)"><PencilSquareIcon class="w-5 h-5" /></button>
     <button class="text-red-600 hover:text-red-700" v-if="actions?.remove" @click="remove(data?.id)"><TrashIcon class="w-5 h-5" /></button>
-    <button class="text-green-600 hover:text-green-700" v-if="actions?.accept" @click="accept(data?.id)"><CheckCircleIcon class="w-5 h-5" /></button>
-    <button class="text-red-600 hover:text-red-700" v-if="actions?.reject" @click="reject(data?.id)"><XCircleIcon class="w-5 h-5" /></button>
   </span>
 </template>
 
@@ -21,8 +19,8 @@ const { $event } = useNuxtApp();
 const add = () => {
   $event("table-add-object");
 }; // add item
-const modify = (id: Number) => {
-  $event("table-modify-object", { id: id });
+const modify = (data: Object) => {
+  $event("table-modify-object", data);
 }; // modify item
 const remove = (id: Number) => {
   $event("table-remove-object", { id: id });
