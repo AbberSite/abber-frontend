a7753938c2a1153b3a6d493d1cf34b364bad5c1e
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-07-22 10:19:33 +0000

    feat(dashboard): add expressors page to demo

--- pages/dashboardv2/expressors/index.vue
+++ pages/dashboardv2/expressors/index.vue
@@ -13,15 +13,27 @@
         </template>
     </div>
   </div>
+  <ClientOnly>
+    <ModifyModal @close="showModifyModal = false" v-if="showModifyModal" >
+        <Selector label="اختر الاجراء" :options="[{value: 'agree', text: 'الموافقة على طلب التعديل'}, {value: 'disagree', text: 'رفض طلب المعبر'}, {value: 'needEdit', text: 'طلب تعديل البيانات'}]" v-model="selected"/>
+        <PrimaryButton class="w-full my-2">متابعة</PrimaryButton>
+    </ModifyModal>
+  </ClientOnly>
 </template>
 
 <script setup lang="ts">
 let currentTab = ref('tab0');
+const {$listen} = useNuxtApp();
+let showModifyModal = ref(false);
+$listen('table-modify-object', (data: object)=> {
+    showModifyModal.value = true;
+})
+const selected = ref('');
 const expressors = [
-    { first_name: 'هيثم صالح', email: 'lsdkjf@glk.co', emailVerified: false, phone: '00971554455', phoneVerified: true, id: 122 },
-    { first_name: 'هيثم صالح', email: 'lsdkjf@glk.co', emailVerified: false, phone: '00971554455', phoneVerified: true, id: 123 },
-    { first_name: 'هيثم صالح', email: 'lsdkjf@glk.co', emailVerified: false, phone: '00971554455', phoneVerified: true, id: 55 },
-    { first_name: 'هيثم صالح', email: 'lsdkjf@glk.co', emailVerified: false, phone: '00971554455', phoneVerified: true, id: 45 },
-    { first_name: 'هيثم صالح', email: 'lsdkjf@glk.co', emailVerified: false, phone: '00971554455', phoneVerified: true, id: 45 },
+    { first_name: 'هيثم صالح', email: 'lsdkjf@glk.co', emailVerified: false, phone: '00971554455', content: 'لقد قمت بتحديث بيانات الخاصة بي', phoneVerified: true, id: 122 },
+    { first_name: 'هيثم صالح', email: 'lsdkjf@glk.co', emailVerified: false, phone: '00971554455', content: 'لقد قمت بتحديث بيانات الخاصة بي', phoneVerified: true, id: 123 },
+    { first_name: 'هيثم صالح', email: 'lsdkjf@glk.co', emailVerified: false, phone: '00971554455', content: 'لقد قمت بتحديث بيانات الخاصة بي', phoneVerified: true, id: 55 },
+    { first_name: 'هيثم صالح', email: 'lsdkjf@glk.co', emailVerified: false, phone: '00971554455', content: 'لقد قمت بتحديث بيانات الخاصة بي', phoneVerified: true, id: 45 },
+    { first_name: 'هيثم صالح', email: 'lsdkjf@glk.co', emailVerified: false, phone: '00971554455', content: 'لقد قمت بتحديث بيانات الخاصة بي', phoneVerified: true, id: 45 },
 ]
 </script>
