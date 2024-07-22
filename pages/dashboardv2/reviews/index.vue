816a5e466e96c87e053b9424c120e56a622238c7
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-07-22 10:46:48 +0000

    feat: created reviews page as demo

---
+++ pages/dashboardv2/reviews/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="relative -mt-2 pb-12">
+    <p class="text-sm font-medium text-gray-800">الإحصائيات</p>
+    <p class="text-lg font-semibold">التقييمات</p>
+  </div>
+  <DashboardTablesTable :head-items="{first_name: 'المستخدم', registration: 'التسجيل', payment: 'الدفع', support: 'الدعم', findExpressors: 'العثور على معبر', reused: 'إعادة الاستخدام', nominations: 'ترشيح الاخرين', comment: 'التعليق'}" :body-items="reviews"/>
+</template>
+
+
+<script setup lang="ts">
+    const reviews = ref([
+    {
+        first_name: 'الشيخ',
+        registration: '5',
+        payment: '4',
+        support: '5',
+        findExpressors: '5',
+        reused: true,
+        nominations: true,
+        comment: 'انصح بتطويره بشكل افضل من الان'
+    },
+    {
+        first_name: 'أحمد',
+        registration: '4',
+        payment: '4',
+        support: '4',
+        findExpressors: '4',
+        reused: false,
+        nominations: true,
+        comment: 'تجربة جيدة بشكل عام'
+    },
+    {
+        first_name: 'خالد',
+        registration: '5',
+        payment: '5',
+        support: '5',
+        findExpressors: '4',
+        reused: false,
+        nominations: false,
+        comment: 'تجربة تحتاج لتحسينات'
+    },
+    {
+        first_name: 'سعيد',
+        registration: '5',
+        payment: '5',
+        support: '5',
+        findExpressors: '5',
+        reused: true,
+        nominations: true,
+        comment: 'أفضل تجربة حصلت عليها'
+    }
+]);
+
+</script>
