be8be9a6b6ed40b845ec7fa165c4b484e25fede0
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-07-23 14:05:54 +0000

    added expressors-support and expressors-to-support

---
+++ pages/dashboardv2/expressors-support/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <DashboardTitle department="الحسابات" title="مدراء العلاقات" />
+    <DashboardTablesTable :head-items="{manager: 'ادارة', expressors: 'المعبرون'}" :body-items="list" :actions="{remove: true, modify: true}"/>
+</template>
+
+
+<script setup>
+const list = ref([
+    { manager: 'هيثم', expressors: ['طه', 'الشيخ'] },
+    { manager: 'هيثم', expressors: ['طه', 'الشيخ'] },
+    { manager: 'هيثم', expressors: ['طه', 'الشيخ'] },
+])
+</script>
