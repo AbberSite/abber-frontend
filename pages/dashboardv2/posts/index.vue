b5884efb5347fdcdbe939e34fab4c4ec9fceaaef
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-07-23 11:35:02 +0000

    added a problem of ckeditor

--- pages/dashboardv2/posts/index.vue
+++ pages/dashboardv2/posts/index.vue
@@ -6,41 +6,16 @@
   <DashboardTab :tabs="[{name: 'المقالات', value: 'posts'}, {name: 'التصنيفات', value: 'categories'}]" v-model:model-value="currentTab"/>
   <div class="pt-2">
     <template v-if="currentTab == 'posts'">
-        <DashboardTablesTable :head-items="{ username: 'المستخدم', title: 'عنوان المقال', pushed: 'مقبولة' }" :body-items="posts" :actions="{ modify: true, remove: true, view: {path: '/blog/'}}" addButton/>
+      <DashboardPosts/>
     </template>
     <template v-else >
-        <DashboardTablesTable :head-items="{ cateTitle: 'اسم التصنيف' }" :body-items="posts" :actions="{ modify: true, remove: true, view: {path: '/blog/'}}"/>
+      <DashboardPostsCategories/>
     </template>
   </div>
-  <ClientOnly>
-    <ModifyModal v-if="showModify" @close="showModify = false;" title="تعديل المقال">
-        <TextInput label="عنوان المقال" />
-        <TextInput label="رابط المقال" />
-        <TextInput label="صورة المقال" :type="'file'" />
-        <DashboardDatePickerInput label="تاريخ النشر"/>
-        <TextInput label="وصف المقال" />
-        <Selector :options="[ {value: 'one', text: 'حلم مبرمج'} ]" label="التصنيف" />
-        <Checkbox label="هل المقال نشط؟" :value="false" v-model:model-value="showModify" class="my-2" />
-        <textarea contenteditable="true"></textarea>
-    </ModifyModal>
-  </ClientOnly>
 </template>
 
 <script setup lang="ts">
-const {$listen} = useNuxtApp();
 const currentTab = ref('posts'); 
-const showModify = ref(false);
 
-$listen('table-modify-object', (data)=> {
-    showModify.value = true;
-});
-$listen('open_add_window', ()=> showModify.value = true)
-const posts = ref([
-    {cateTitle: 'صنفي المفضل', username: 'الشيخ المختار', title: 'لكل امرإ من دهره ما  تعودى', pushed: true},
-    {cateTitle: 'صنفي المفضل', username: 'الشيخ المختار', title: 'لكل امرإ من دهره ما  تعودى', pushed: false},
-    {cateTitle: 'صنفي المفضل', username: 'الشيخ المختار', title: 'لكل امرإ من دهره ما  تعودى', pushed: false},
-    {cateTitle: 'صنفي المفضل', username: 'الشيخ المختار', title: 'لكل امرإ من دهره ما  تعودى', pushed: true},
-    {cateTitle: 'صنفي المفضل', username: 'الشيخ المختار', title: 'لكل امرإ من دهره ما  تعودى', pushed: false},
-    {cateTitle: 'صنفي المفضل', username: 'الشيخ المختار', title: 'لكل امرإ من دهره ما  تعودى', pushed: true},
-])
+
 </script>
