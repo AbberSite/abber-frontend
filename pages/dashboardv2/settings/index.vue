7f8308c06d9f8e03a5c8f2ccf96cb512262ed9b5
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-07-24 08:51:27 +0000

    feat: Complete a portion of the Public Settings module

---
+++ pages/dashboardv2/settings/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <DashboardTitle department="الاعدادات" title="إعدادات عامة"/>
+    <DashboardTab :tabs="[{value: 'tab0', name: 'القوائم'}, {value: 'tab1', name: 'الشروط والاحكام'}, {value: 'tab2', name: 'الصور الافتراضية'}]" v-model:model-value="currentTab"/>
+    <div class="pt-4">
+        <template v-if="currentTab == 'tab0'">
+            <DashboardSettingsPublicSettings />
+        </template>
+        <template v-else-if="currentTab == 'tab1'">
+            <span class="font-semibold text-gray-800" >انتظر الانتهاء من الeditor </span>
+        </template>
+        <template v-else>
+            <DashboardSettingsDefaultImages/>
+        </template>
+    </div> 
+</template>
+
+<script setup lang="ts">
+const currentTab = ref('tab0');
+
+</script>
