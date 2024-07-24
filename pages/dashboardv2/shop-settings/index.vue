1eafc1790465e752d4efce7fb7880c8bba8db9fa
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-07-24 11:53:42 +0000

    feat: Add Shop Settings components and page

---
+++ pages/dashboardv2/shop-settings/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <DashboardTitle department="الاعدادات" title="إعدادات الموقع" />
+  <DashboardTab
+    :tabs="[
+      { value: 'tab0', name: 'بيانات الموقع' },
+      { value: 'tab1', name: 'بيانات التواصل الاجتماعي' },
+      { value: 'tab2', name: 'السياسات' },
+      { value: 'tab3', name: 'اعدادات API' },
+      { value: 'tab4', name: 'البيانات المالية' },
+      { value: 'tab5', name: 'رسوم طرق الدفع' },
+      { value: 'tab6', name: 'النسخ الاحتياطي' },
+      { value: 'tab7', name: 'إعدادات الباقات' },
+      { value: 'tab8', name: 'Orders Packages Settings' },
+    ]"
+    v-model:model-value="currentTab"
+  />
+  <div class="pt-4">
+    <template v-if="currentTab == 'tab0'">
+      <DashboardShopSettingsSiteData />
+    </template>
+    <template v-else-if="currentTab == 'tab1'">
+      <DashboardShopSettingsSocailData />
+    </template>
+    <template v-else-if="currentTab == 'tab2'">
+      <DashboardShopSettingsPolicies />
+    </template>
+    <template v-else-if="currentTab == 'tab3'">
+      <DashboardShopSettingsAPI />
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+const currentTab = ref("tab0");
+</script>
