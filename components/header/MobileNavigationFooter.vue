fe7bf5bc98a5fca9bad8ecb95ed012f110ac5a95
Abdullah Alnuaimi <abdullahalnuaimi36@gmail.com>
2024-07-28 12:47:21 +0300

    Fixed issues design part 5

--- components/header/MobileNavigationFooter.vue
+++ components/header/MobileNavigationFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="py-6">
         <NuxtLink
-            class="-mx-3 block rounded-lg px-3 py-3 font-medium hover:bg-gray-50"
+            class="-mx-3 block rounded-lg px-3 py-4 font-medium hover:bg-gray-50"
             @click="$emit('navigate', { name: 'accounts-sms' })"
             >
             تسجيل الدخول
@@ -9,22 +9,22 @@
     </div>
     <div class="space-y-3 py-6">
         <NuxtLink
-            class="-mx-3 block rounded-lg px-3 py-3 font-medium hover:bg-gray-50"
+            class="-mx-3 block rounded-lg px-3 py-4 font-medium hover:bg-gray-50"
             @click="$emit('navigate', { name: 'about' })"
             >من نحن</NuxtLink
         >
         <NuxtLink
-            class="-mx-3 block rounded-lg px-3 py-3 font-medium hover:bg-gray-50"
+            class="-mx-3 block rounded-lg px-3 py-4 font-medium hover:bg-gray-50"
             @click="$emit('navigate', {name: 'index', hash: '#faqs'})"
             >الأسئلة الشائعة</NuxtLink
         >
         <NuxtLink
-            class="-mx-3 block rounded-lg px-3 py-3 font-medium hover:bg-gray-50"
+            class="-mx-3 block rounded-lg px-3 py-4 font-medium hover:bg-gray-50"
             @click="$emit('navigate', { name: 'terms' })"
             >الشروط و الأحكام</NuxtLink
         >
         <NuxtLink
-            class="-mx-3 block rounded-lg px-3 py-3 font-medium hover:bg-gray-50"
+            class="-mx-3 block rounded-lg px-3 py-4 font-medium hover:bg-gray-50"
             @click="$emit('navigate', { name: 'user-agreement' })"
             >إتفاقية المستخدم</NuxtLink
         >
