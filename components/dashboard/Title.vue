920029da5fe6530e5f6ae0d105c73ca68219d43c
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-07-30 17:54:38 +0000

    feat: Use DashboardTitle to automatically set the title for the dashboard pages

--- components/dashboard/Title.vue
+++ components/dashboard/Title.vue
@@ -1,4 +1,7 @@
 <template>
+  <Head>
+        <Title>عبر - {{ titleComputed }}</Title>
+    </Head>
     <div class="relative -mt-2 pb-12">
     <p class="text-sm font-medium text-gray-600">{{ department }}</p>
     <p class="text-lg font-medium">{{ title }}</p>
@@ -7,5 +10,11 @@
 </template>
 
 <script setup lang="ts">
-defineProps<{department: string, title:string}>();
+const props = defineProps<{department: string, title:string}>();
+const titleComputed = computed(()=> {
+  if(useRoute().path.includes('dashboardv2')){
+    return `لوحة التحكم ${props.title == 'لوحة التحكم'? ``: `- ${props.title}`}`
+  }
+  return props.title;
+})
 </script>
