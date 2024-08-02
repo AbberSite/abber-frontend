d3d4a0b08a5b6197ce69a10a847ea3c041755ac9
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-08-02 13:14:56 +0000

    update dashboard style

--- components/dashboard/Analytics/Charts.vue
+++ components/dashboard/Analytics/Charts.vue
@@ -1,20 +1,36 @@
 <template>
-  <div class="h-fit space-y-6 overflow-hidden pt-6 md:pt-0 lg:space-y-6">
+  <div
+    class="h-fit space-y-4 md:col-span-3 overflow-hidden pt-6 md:pt-0 lg:space-y-6"
+  >
     <div class="space-y-8 rounded-lg border border-gray-200 py-6">
-      <h3 class="px-6 font-medium text-gray-500 xl:text-lg">إحصائيات الطلبات</h3>
+      <h3 class="px-6 font-medium text-gray-500 sm:px-4 lg:px-6 xl:text-lg">
+        إحصائيات الطلبات
+      </h3>
       <Line :data="chartData" :options="chartOptions" />
     </div>
-    <div class="space-y-8 rounded-lg border border-gray-200 py-6">
-      <h3 class="px-6 font-medium text-gray-500 xl:text-lg">الطلبات</h3>
-      <Bar :data="chartData" :options="chartOptions" />
+    <div class="grid grid-cols-2 gap-x-4 lg:gap-x-6">
+      <div class="space-y-8 rounded-lg border border-gray-100 py-6">
+        <h3 class="px-6 font-medium text-gray-500 sm:px-4 lg:px-6 xl:text-lg">
+          الطلبات
+        </h3>
+        <Pie :data="pieChart" />
+      </div>
+      <div class="space-y-8 rounded-lg border border-gray-100 py-6">
+        <h3 class="px-6 font-medium text-gray-500 sm:px-4 lg:px-6 xl:text-lg">
+          الطلبات
+        </h3>
+        <Doughnut :data="pieChart" />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Line, Bar } from "vue-chartjs";
+const props = defineProps<{ datasets: Array<{}> }>();
+import { Line, Pie, Doughnut } from "vue-chartjs";
 import {
   Chart as ChartJS,
+  ArcElement,
   Title,
   Tooltip,
   Legend,
@@ -23,9 +39,31 @@ import {
   CategoryScale,
   LinearScale,
 } from "chart.js";
+const generateRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 
+const generateColors = (dataLength) => {
+  return Array.from({ length: dataLength }, () => generateRandomColor());
+};
+const pieDataset = computed(() => 
+  props.datasets.map((el) => ({
+    ...el,
+    backgroundColor: generateColors(3),
+    borderColor: '',
+    borderWidth: ''
+  }))
+);
+
+console.log(pieDataset.value)
 // Register the required components for Chart.js
 ChartJS.register(
+  ArcElement,
   Title,
   Tooltip,
   Legend,
@@ -34,11 +72,38 @@ ChartJS.register(
   CategoryScale,
   LinearScale
 );
-const props = defineProps<{datasets: Array<{}>}>()
 let chartData = reactive({
-  labels: ["الأثنين", "الثلاثاء", "الأربعاء", "الخميس", "الجمعة", "السبت", "الأحد"],
+  labels: [
+    "الأثنين",
+    "الثلاثاء",
+    "الأربعاء",
+    "الخميس",
+    "الجمعة",
+    "السبت",
+    "الأحد",
+  ],
   datasets: props.datasets,
-})
+});
+const pieChart = {
+  labels: [
+  "الأثنين",
+    "الثلاثاء",
+    "الأربعاء",
+    "الخميس",
+    "الجمعة",
+    "السبت",
+    "الأحد",
+  ],
+  datasets: pieDataset.value
+  // datasets: [
+  // {
+  //   label: "جميع الطلبات",
+  //   data: [12, 8, 3, 5, 2, 3, 40],
+  //   borderWidth: 1,
+  //   backgroundColor: generateColors(7)
+  // }
+  // ],
+};
 const chartOptions = {
   scales: {
     y: {
@@ -46,4 +111,6 @@ const chartOptions = {
     },
   },
 };
+
+
 </script>
--- components/dashboard/Analytics/Earings.vue
+++ components/dashboard/Analytics/Earings.vue
@@ -1,27 +1,25 @@
 <template>
-    <div class="space-y-6">
+    <div class="space-y-4 md:col-span-2 lg:space-y-6">
             <div class="h-fit space-y-8 overflow-hidden rounded-lg border border-gray-200 py-6">
-              <div class="space-y-3 px-6">
+              <div class="space-y-3 px-6 sm:px-4 lg:px-6">
                 <h3 class="font-medium text-gray-500 xl:text-lg">{{earingsData[0].title}}</h3>
                 <p class="space-x-1 text-lg font-semibold rtl:space-x-reverse xl:text-xl"><span>{{earingsData[0].amount}}</span><small>ر.س</small></p>
               </div>
-              <div class="grid grid-cols-2 gap-8 px-6 sm:grid-cols-3 md:grid-cols-2 md:gap-x-4 lg:grid-cols-3 xl:gap-x-6">
-                <div class="space-y-6" v-for="({title, amount, down}, index) in earingsData" :key="index" v-show="index != 0">
-                  <h3 class="text-sm font-medium text-gray-500">{{title}}</h3>
+              <div class="grid grid-cols-2 gap-8 px-6 sm:px-4 md:gap-x-4 lg:px-6 xl:gap-x-6">
+                <div class="space-y-4" v-for="(data, index) in earingsData" :key="index" v-show="index != 0">
+                  <h3 class="text-sm font-medium text-gray-500">{{data.title}}</h3>
                   <p class="flex items-center space-x-3 rtl:space-x-reverse">
-                    <span class="flex h-8 w-8 items-center justify-center rounded-md " :class="{'bg-yellow-600/10 text-yellow-800': down, 'bg-green-600/10 text-green-800': !down}" >
-                      <ArrowUpIcon v-if="!down" class="w-[18px] h-[18px]"/>
-                      <ArrowDownIcon v-else class="w-[18px] h-[18px]"/>
-                      </span
-                    ><span class="space-x-1 font-semibold rtl:space-x-reverse lg:text-lg 2xl:text-xl"><span>{{amount}}</span><small>ر.س</small></span>
+                    <span :class="`flex h-8 w-8 items-center justify-center rounded-md ${data.colors}`">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height="18" width="18" v-html="data.svgIcon"></svg> </span>
+                      <span>{{data.amount}}</span><small>{{data.type ?? 'ر.س'}}</small>
                   </p>
                 </div>
               </div>
             </div>
 
             <div class="h-fit overflow-hidden rounded-lg border border-gray-200 py-6">
-              <h3 class="px-6 font-medium text-gray-500 xl:text-lg">إحصائيات الرسوم</h3>
-                <div class="px-6  pt-6">
+              <h3 class="px-6 font-medium text-gray-500 sm:px-4 lg:px-6 xl:text-lg">إحصائيات الرسوم</h3>
+                <div class="px-6 pt-6  sm:px-4 lg:px-6">
                   <div v-for="({title, amount}, i) in ordersInfo" :key="i" class="flex items-center justify-between py-4">
                     <div class="text-sm font-medium text-gray-500 lg:text-base">{{ title }}</div>
                     <div class="text-sm font-medium lg:text-base">{{ amount }} ر.س</div>
@@ -33,12 +31,12 @@
 
 <script setup lang="ts">
 import { ArrowUpIcon, ArrowDownIcon } from '@heroicons/vue/24/outline';
-defineProps<{total: number | string; recieved: number | string; pending: number | string; earingsData: Array<{title: string, amount: number, down?:boolean}>; ordersInfo: Array<{title: string, amount: number}>}>();
+defineProps<{total: number | string; recieved: number | string; pending: number | string; earingsData: Array<{title: string, amount: number, colors: string, svgIcon: string, type: string}>; ordersInfo: Array<{title: string, amount: number}>}>();
 </script>
 
 <style scoped>
 span {
-   @apply font-medium text-gray-600 text-lg;
+   @apply font-medium text-lg;
 }
 h2 {
    @apply font-bold text-xl;
