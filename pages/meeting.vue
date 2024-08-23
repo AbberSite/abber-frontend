77e1f6678218c45e732748723264b1a913e83244
tahaghazy <68954755+tahaghazy@users.noreply.github.com>
2024-08-24 00:24:19 +0300

    add meeting page

---
+++ pages/meeting.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+<client-only>
+  <webrtc :is-host="data.user_type == 'معبر'" room="190b3815"></webrtc> 
+  </client-only>
+  </div>
+</template>
+
+<script setup>
+setPageLayout('none');
+const { data } = useAuth();
+
+</script>
+
+<style>
+
+</style>
