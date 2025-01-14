0d0d2d26a1e1f662f9081f76cb07fc47bd803ad9
Azzam <68954755+tahaghazy@users.noreply.github.com>
2025-01-14 08:52:58 +0200

    refactor: Update checkbox and radiobox styles

--- components/orders/form-steps/cardComponent.vue
+++ components/orders/form-steps/cardComponent.vue
@@ -3,19 +3,25 @@
     <label
       class="relative flex w-full min-w-[12rem] cursor-pointer justify-between rounded-md border px-4 py-4 shadow-sm focus:outline-none"
       :class="modelValue === idOfCard ? 'border-gray-900 ring-1 ring-gray-900' : ''">
+
       <template v-if="multi" >
+        
         <div class="flex items-center space-x-2 rtl:space-x-reverse">
+      <input class="h-5 w-5 flex-shrink-0 appearance-none rounded-full" type="radio" name="type"
+        :checked="idOfCard == modelValue ? true : false" :value="idOfCard" @input="emits('update:modelValue', idOfCard)"
+        @change="selectedOption = idOfCard" />
         <div class="pt-1 text-sm font-medium">{{ title }}</div>
       </div>
       <div class="absolute bottom-0 left-0 overflow-hidden rounded-md text-gray-200"><img
           class="lazyload -translate-x-[1.5em] translate-y-[.7em] opacity-30" width="250px" data-src="/images/payments/section/cards.png" 
           alt="ماستركارد" /></div>
-      <input class="h-5 w-5 flex-shrink-0 appearance-none rounded-full" type="radio" name="type"
-        :checked="idOfCard == modelValue ? true : false" :value="idOfCard" @input="emits('update:modelValue', idOfCard)"
-        @change="selectedOption = idOfCard" />
+
       </template>
       <template v-else >
         <div class="flex items-center space-x-2 rtl:space-x-reverse">
+<input class="h-5 w-5 flex-shrink-0 appearance-none rounded-full" type="radio" name="type"
+        :checked="idOfCard == modelValue ? true : false" :value="idOfCard" @input="emits('update:modelValue', idOfCard)"
+        @change="selectedOption = idOfCard" />
         <img class="lazyload"
           src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTk4IiBoZWlnaHQ9IjE5OCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
           :data-src="logo" :height="height" :width="width" alt="ماستركارد" />
@@ -24,9 +30,7 @@
       <div class="absolute bottom-0 left-0 overflow-hidden rounded-md text-gray-200"><img
           class="lazyload -translate-x-[1em] translate-y-1/8 opacity-30" :data-src="logo" height="64" width="64"
           alt="ماستركارد" /></div>
-      <input class="h-5 w-5 flex-shrink-0 appearance-none rounded-full" type="radio" name="type"
-        :checked="idOfCard == modelValue ? true : false" :value="idOfCard" @input="emits('update:modelValue', idOfCard)"
-        @change="selectedOption = idOfCard" />
+      
       </template>
     </label>
   </div>
