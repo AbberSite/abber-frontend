7b324eca16742c6480b69380896fe65ba8703f71
Azzam <68954755+tahaghazy@users.noreply.github.com>
2025-01-28 02:07:35 +0200

    refactor: Filter available text communication services for selection

--- components/orders/form-steps/Service.vue
+++ components/orders/form-steps/Service.vue
@@ -1,12 +1,10 @@
 <template>
-
   <Head>
     <title>عبر - طلب تعبير حلم - إختيار المعبر</title>
   </Head>
 
   <div>
     <div>
-
       <fieldset class="space-y-5">
         <div class="is-scroll max-h-[400px] space-y-5 overflow-y-auto p-1" id="list-expressors">
           <template v-if="loading">
@@ -18,13 +16,12 @@
           <template v-else>
             <template v-if="state.data?.type == 'text_communication'">
               <FormStepsServiceSelectUrgentOrderService @click.once="submit(0)" v-model="selectedService" />
-              <FormStepsServiceSelectServiceRadioButton v-for="service in textCommunicationServices" 
-                v-model="selectedService" @click.once="submit(service.id, service.service_prices.text_price)"
-                :service="service" :type="1" />
+              <FormStepsServiceSelectServiceRadioButton v-for="service in textCommunicationServices.filter((service) => service?.work_hour?.is_available_now)" v-model="selectedService" @click.once="submit(service.id, service.service_prices.text_price)" :service="service" :type="1" />
+              <span class="text-center text-sm py-0.5 inline-block w-full"> قد يتأخر المعبر في الرد </span>
+              <FormStepsServiceSelectServiceRadioButton v-for="service in textCommunicationServices.filter((service) => !service?.work_hour?.is_available_now)" v-model="selectedService" @click.once="submit(service.id, service.service_prices.text_price)" :service="service" :type="1" />
             </template>
             <template v-else>
-              <FormStepsServiceSelectServiceRadioButton v-for="service in videoServices" v-model="selectedService"
-                @click.once="submitVideo(service.id)" :service="service" :type="0" />
+              <FormStepsServiceSelectServiceRadioButton v-for="service in videoServices" v-model="selectedService" @click.once="submitVideo(service.id)" :service="service" :type="0" />
             </template>
           </template>
         </div>
@@ -34,12 +31,12 @@
 </template>
 
 <script setup lang="ts">
-import type { OrderForm } from '~/types';
+import type { OrderForm } from "~/types";
 
-const { state, next } = useFormWizard<OrderForm>('order');
+const { state, next } = useFormWizard<OrderForm>("order");
 
 const { status } = useAuth();
-const emits = defineEmits(['next']);
+const emits = defineEmits(["next"]);
 
 const loading = ref(false);
 
@@ -50,13 +47,12 @@ const { fetchAll } = useServicesStore();
 const { services, textCommunicationServices, videoServices } = storeToRefs(useServicesStore());
 
 onMounted(async () => {
-
-  let listExpressorElement = document.getElementById('list-expressors') as Element;
+  let listExpressorElement = document.getElementById("list-expressors") as Element;
   setTimeout(() => {
-    listExpressorElement.scrollTo({ behavior: 'smooth', top: listExpressorElement.scrollHeight / 2 })
+    listExpressorElement.scrollTo({ behavior: "smooth", top: listExpressorElement.scrollHeight / 2 });
   }, 800);
   setTimeout(() => {
-    listExpressorElement.scrollTo({ behavior: 'smooth', top: 0 })
+    listExpressorElement.scrollTo({ behavior: "smooth", top: 0 });
   }, 2000);
 
   if (services.value.length != 0) return;
@@ -64,42 +60,36 @@ onMounted(async () => {
   loading.value = true;
   await fetchAll();
   loading.value = false;
-
 });
 
 function submit(service_id: number) {
   selectedService.value = service_id;
 
-  if (status.value == 'loading') return
+  if (status.value == "loading") return;
   if (!service_id) {
-    if (status.value == 'authenticated') {
+    if (status.value == "authenticated") {
       next({
-        nextStepId: 'payment', data: { service_id: 0, orders: [], selectedServices: [] }
+        nextStepId: "payment",
+        data: { service_id: 0, orders: [], selectedServices: [] },
       });
       return;
     } else {
       next({
-        nextStepId: 'authentication-method', data: { service_id: 0, orders: [], selectedServices: [] }
-      })
+        nextStepId: "authentication-method",
+        data: { service_id: 0, orders: [], selectedServices: [] },
+      });
     }
     return;
   }
-  next({ nextStepId: 'my-urgent-order-service', data: { service_id: selectedService.value } });
-
-
+  next({ nextStepId: "my-urgent-order-service", data: { service_id: selectedService.value } });
 }
 
 function submitVideo(service_id: number) {
   selectedService.value = service_id;
 
-
-  if (status.value == 'loading') return
-  if (status.value == 'authenticated')
-    next({ nextStepId: 'payment', data: { service_id: selectedService.value } });
-  else
-    next({ nextStepId: 'authentication-method', data: { service_id: selectedService.value } });
-
-
+  if (status.value == "loading") return;
+  if (status.value == "authenticated") next({ nextStepId: "payment", data: { service_id: selectedService.value } });
+  else next({ nextStepId: "authentication-method", data: { service_id: selectedService.value } });
 }
 </script>
 
