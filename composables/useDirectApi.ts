02f1315196832c549912dd76c1a5b192d23b0639
Taha Ghazi <tahaghazt164@gmail.com>
2025-04-07 19:05:04 +0200

    Fix issue

--- composables/useDirectApi.ts
+++ composables/useDirectApi.ts
@@ -1,16 +1,18 @@
-import type { NitroFetchRequest } from 'nitropack';
-
-export default async <T = unknown, R extends NitroFetchRequest = NitroFetchRequest>(
-  request: Parameters<typeof $fetch<T, R>>[0],
-  options?: Partial<Parameters<typeof $fetch<T, R>>[1]>
+export default async <T = unknown>(
+  request: Parameters<typeof $fetch<T>>[0],
+  options?: Partial<Parameters<typeof $fetch<T>>[1]>
 ): ReturnType<typeof $fetch> => {
   const { status, rawToken } = useAuthState();
-  return $fetch<T, R>("/api/custom-api-endpoint/fetch-api/" as R, {
+
+  return $fetch<T>("/api/custom-api-endpoint/fetch-api/", {
     ...options,
     headers: {
       nuxtapiurl: request.toString(),
-      Authorization: status.value != 'unauthenticated' && rawToken.value ? `JWT ${rawToken.value}` : '',
-      ...options?.headers
-    }
+      Authorization:
+        status.value !== 'unauthenticated' && rawToken.value
+          ? `JWT ${rawToken.value}`
+          : '',
+      ...options?.headers,
+    },
   });
 };
