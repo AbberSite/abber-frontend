90ed58eca23829f55b007c3c366341d3610c0ca2
Taha Ghazi <tahaghazt164@gmail.com>
2025-04-07 18:58:57 +0200

    Fix issue

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -1,16 +1,17 @@
-import { type NitroFetchRequest } from 'nitropack';
-
-export default async <T = unknown, R extends NitroFetchRequest = NitroFetchRequest>(
-    request: Parameters<typeof $fetch<T, R>>[0],
-    options?: Partial<Parameters<typeof $fetch<T, R>>[1]>
+export default async <T = unknown>(
+  request: Parameters<typeof $fetch<T>>[0],
+  options?: Partial<Parameters<typeof $fetch<T>>[1]>
 ): ReturnType<typeof $fetch> => {
-    const { status, rawToken } = useAuthState();
+  const { status, rawToken } = useAuthState();
 
-    return $fetch<T, R>(request, {
-        ...options,
-        headers: {
-            Authorization: status.value != 'unauthenticated' && rawToken.value ? `JWT ${rawToken.value}` : '',
-            ...options?.headers
-        }
-    });
+  return $fetch<T>(request, {
+    ...options,
+    headers: {
+      Authorization:
+        status.value !== 'unauthenticated' && rawToken.value
+          ? `JWT ${rawToken.value}`
+          : '',
+      ...options?.headers,
+    },
+  });
 };
