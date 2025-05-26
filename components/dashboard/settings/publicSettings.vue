4e59978e146e26c312f77afd6b6d8ecead6c0e27
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2025-05-26 17:35:15 +0000

    feat(dashboard): complete public settings section

--- components/dashboard/settings/publicSettings.vue
+++ components/dashboard/settings/publicSettings.vue
@@ -1,34 +1,71 @@
 <template>
-    <DashboardTablesTable :head-items="{first: 'النص الاول', second: 'النص الثاني'}" :body-items="settingsList" :actions="{modify: true, remove: true}" addButton />
-    <ModifyModal @close="showModifyModal = false" v-if="showModifyModal" :title="modifyTitle">
+    <DashboardTablesTable :head-items="{caption_1: 'النص الاول', caption_2: 'النص الثاني'}" :body-items="partitions" :actions="{modify: true, remove: true}" addButton :loading="loading" />
+    <ModifyModal @close="showModifyModal = false" v-if="showModifyModal" :title="title">
         <div class="mt-3" >
-            <TextInput label="الترتيب" />
-            <TextInput label="النص الاول" />
-            <TextInput label="النص الثاني" />
-            <TextInput label="الايقونة" />
-            <PrimaryButton class="w-full my-2" >احفظ</PrimaryButton>
+            <TextInput label="الترتيب" v-model="partitionData.index" />
+            <TextInput label="النص الاول" v-model="partitionData.caption_1" />
+            <TextInput label="النص الثاني" v-model="partitionData.caption_2" />
+            <TextInput label="الايقونة" type="textarea" v-model="partitionData.svg_icon" dir="ltr" />
+            <PrimaryButton class="w-full my-2" :loading="loading" @click="updateSettingsPartition" >احفظ</PrimaryButton>
         </div>
     </ModifyModal>
+    <ModifyModal @close="showCreateModal = false" v-if="showCreateModal" :title="title">
+        <div class="mt-3" >
+            <TextInput label="الترتيب" v-model="partitionData.index" />
+            <TextInput label="النص الاول" v-model="partitionData.caption_1" />
+            <TextInput label="النص الثاني" v-model="partitionData.caption_2" />
+            <TextInput label="الايقونة" type="textarea" v-model="partitionData.svg_icon" dir="ltr" />
+            <PrimaryButton class="w-full my-2" :loading="loading" @click="createSettingsPartition" >إنشاء</PrimaryButton>
+        </div>
+    </ModifyModal>
+    <ConfirmDialog v-if="showDeleteConfirm" @close="cancelDelete" @continue="confirmDelete" title="تأكيد الحذف"
+        descritpion="هل أنت متأكد أنك تريد حذف هذه القائمة؟ لا يمكن التراجع عن هذا الإجراء." />
 </template>
 <script setup lang="ts">
 import { useDashboardSettingsStore } from '~/stores/dashboard/dashboardSettings';
 const {$listen} = useNuxtApp();
-const { settings, loading } = storeToRefs(useDashboardSettingsStore());
-const { getSettings } = useDashboardSettingsStore();
-let showModifyModal = ref(false);
-let modifyTitle = ref('');
-const settingsList = ref([
-    { first: 'سجل بالطريقة المناسبة لك', second: 'أختر وسيلة التسجيل الأكثر ملائمة لك وكن على تواصل دائم' },
-    { first: 'سجل بالطريقة المناسبة لك', second: 'أختر وسيلة التسجيل الأكثر ملائمة لك وكن على تواصل دائم' },
-    { first: 'سجل بالطريقة المناسبة لك', second: 'أختر وسيلة التسجيل الأكثر ملائمة لك وكن على تواصل دائم' },
-]);
+const { partitions, loading, partitionData, showCreateModal, showModifyModal, title, showDeleteConfirm } = storeToRefs(useDashboardSettingsStore());
+const { getSettings, getSettingsPartitions, createSettingsPartition, removeSettingsPartition, updateSettingsPartition } = useDashboardSettingsStore();
+const deleteId = ref<number|null>(null);
 
 $listen('table-modify-object', (data)=> {
+    partitionData.value = data;
     showModifyModal.value = true;
-    modifyTitle.value = 'تعديل القائمة'
+    title.value = 'تعديل القائمة'
 });
-$listen('open_add_window', ()=> {showModifyModal.value = true; modifyTitle.value = "إنشاء قائمة جديدة";});
+$listen('open_add_window', ()=> {
+    resetPartitionData();
+    showCreateModal.value = true; 
+    title.value = "إنشاء قائمة جديدة";
+});
+// Listen for remove event
+$listen('table-remove-object', (data) => {
+    deleteId.value = data.id;
+    showDeleteConfirm.value = true;
+});
+
+function confirmDelete() {
+    if (deleteId.value !== null) {
+        removeSettingsPartition(deleteId.value);
+    }
+    showDeleteConfirm.value = false;
+    deleteId.value = null;
+}
+function cancelDelete() {
+    showDeleteConfirm.value = false;
+    deleteId.value = null;
+}
+
+function resetPartitionData() {
+    partitionData.value = {
+        index: '',
+        caption_1: '',
+        caption_2: '',
+        svg_icon: ''
+    };
+}
 onMounted(() => {
     getSettings();
+    getSettingsPartitions();
 });
 </script>
--- stores/dashboard/dashboardSettings.ts
+++ stores/dashboard/dashboardSettings.ts
@@ -1,7 +1,12 @@
 class dashboardSettings {
   loading = ref(false);
   settings = ref<{ [key: string]: any }>({});
-
+  partitions = ref<[]>([]);
+  partitionData = ref<{ [key: string]: any }>({});
+  showModifyModal = ref(false);
+  showCreateModal = ref(false);
+  title = ref('');
+  showDeleteConfirm = ref(false);
   
   getSettings = async () => {
     return new Promise(async (resolve, reject) => {
@@ -10,6 +15,42 @@ class dashboardSettings {
         const data = await useDirectApi('/settings/dashboard-settings/');
         this.settings.value = data.results[0];
         this.loading.value = false;
+        resolve(data);
+      } catch (error) {
+        this.loading.value = false;
+        reject(error);
+      }
+    });
+  };
+  getSettingsPartitions = async () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        this.loading.value = true;
+        const data = await useDirectApi('/settings/partitions/');
+        this.partitions.value = data.results;
+        this.loading.value = false;
+        resolve(data);
+      } catch (error) {
+        this.loading.value = false;
+        reject(error);
+      }
+    });
+  };
+  createSettingsPartition = async () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        this.loading.value = true;
+        const data = await useDirectApi('/settings/partitions/', {
+          method: 'POST',
+          body: this.partitionData.value
+        });
+        this.getSettingsPartitions();
+        this.loading.value = false;
+        this.showCreateModal.value = false;
+        useNotification({
+          type: 'success',
+          content: 'تم إنشاء القسم بنجاح.',
+        });
         resolve(data);
       } catch (error) {
         this.loading.value = false;
@@ -38,6 +79,59 @@ class dashboardSettings {
         useNotification({
           type: 'danger',
           content: 'فشلت عملية تحديث الإعدادات.',
+        });
+        reject(error);
+      }
+    });
+  };
+
+  updateSettingsPartition = async () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        this.loading.value = true;
+        const id = this.partitionData.value.id;
+        const data = await useDirectApi(`/settings/partitions/${id}/`, {
+          method: 'PATCH',
+          body: this.partitionData.value
+        });
+        await this.getSettingsPartitions();
+        this.loading.value = false;
+        this.showModifyModal.value = false;
+        useNotification({
+          type: 'success',
+          content: 'تم تحديث القسم بنجاح.',
+        });
+        resolve(data);
+      } catch (error) {
+        this.loading.value = false;
+        useNotification({
+          type: 'danger',
+          content: 'فشلت عملية تحديث القسم.',
+        });
+        reject(error);
+      }
+    });
+  };
+
+  removeSettingsPartition = async (id: number) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        this.loading.value = true;
+        await useDirectApi(`/settings/partitions/${id}/`, {
+          method: 'DELETE'
+        });
+        await this.getSettingsPartitions();
+        this.loading.value = false;
+        useNotification({
+          type: 'success',
+          content: 'تم حذف القسم بنجاح.',
+        });
+        resolve(true);
+      } catch (error) {
+        this.loading.value = false;
+        useNotification({
+          type: 'danger',
+          content: 'فشلت عملية حذف القسم.',
         });
         reject(error);
       }
