a402db797fd4c70ddb9e4c2d1753b4c9a45dc623
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2025-05-27 14:52:12 +0000

    completed socialdata in settings of the site

--- components/dashboard/ShopSettings/SocailData.vue
+++ components/dashboard/ShopSettings/SocailData.vue
@@ -1,9 +1,21 @@
 <template>
-    <div class="grid grid-cols-1 gap-8 sm:grid-cols-2">
-        <TextInput label="فيسبوك:"/>
-        <TextInput label="سناب شات:"/>
-    <TextInput label="تويتر:"/>
-        <TextInput label="استاكرام:"/>
-    </div>
-    <PrimaryButton class="my-2">حفظ</PrimaryButton>
+    <template v-if="loading">
+        <SkeletonsSocialData/>
+    </template>
+    <template v-else>
+        <div class="grid grid-cols-1 gap-8 sm:grid-cols-2">
+            <TextInput label="فيسبوك:" v-model="settings.social_settings.facebook"/>
+            <TextInput label="سناب شات:" v-model="settings.social_settings.snapchat"/>
+            <TextInput label="تويتر:" v-model="settings.social_settings.twitter"/>
+            <TextInput label="استاغرام:" v-model="settings.social_settings.instagram"/>
+        </div>
+        <PrimaryButton class="my-2" :loading="loading" @click="updateSettings(2, {social_settings: {...settings.social_settings}})" >حفظ</PrimaryButton>
+    </template>
+
 </template>
+
+<script setup lang="ts">
+import { useDashboardSettingsStore } from '~/stores/dashboard/dashboardSettings';
+const { loading, settings } = storeToRefs(useDashboardSettingsStore());
+const { updateSettings } = useDashboardSettingsStore();
+</script>
