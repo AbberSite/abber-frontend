e132516f7a8804ae41bef33eb371a25355366bc2
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2025-05-31 10:55:32 +0000

    feat: add beautiful rich text editor powered by CKEditor

--- components/shared/Editor.vue
+++ components/shared/Editor.vue
@@ -1,5 +1,6 @@
 <template>
-  <label v-if="label" class="block mb-1 font-medium">{{ label }}</label>
+  <div>
+    <label v-if="label" class="block mb-1 font-medium">{{ label }} :</label>
   <ckeditor
     :editor="editor"
     :model-value="modelValue"
@@ -7,8 +8,9 @@
     :config="{
       language: 'ar',
       translations: [coreTranslations]
-    }"
+    }" 
   />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -22,3 +24,12 @@ const emits = defineEmits(['update:modelValue']);
 const editor = $ckeditor.classicEditor;
 import coreTranslations from 'ckeditor5/translations/ar.js';
 </script>
+
+<style scoped>
+:deep(.ck-content) {
+  padding: 0px 20px;
+}
+:deep(.ck-content) {
+  @apply prose min-h-[250px] min-w-full;
+}
+</style>
