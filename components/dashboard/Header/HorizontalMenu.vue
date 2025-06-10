f0c3dcec3198d1e49d136fbdde913111f72a6ff6
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2025-06-10 14:53:56 +0000

    components/dashboard/Header/HorizontalMenu.vue

--- components/dashboard/Header/HorizontalMenu.vue
+++ components/dashboard/Header/HorizontalMenu.vue
@@ -214,10 +214,10 @@
         type="button">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
           height="20" width="20">
-          <path stroke-linecap="round" stroke-linejoin="round"
-            d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z">
-          </path>
-        </svg><span>الإعدادات</span>
+        <path stroke-linecap="round" stroke-linejoin="round"
+          d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z">
+        </path>
+      </svg><span>الإعدادات</span>
         <svg class="transition-all group-hover:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none"
           viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" height="12" width="12">
           <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5"></path>
@@ -306,29 +306,28 @@ onMounted(async function () {
   const submenu = document.querySelectorAll('.submenu');
   const menuWrapper = document.querySelector('.all_menu_group');
 
-  const updatePosition = function (topPosition = 0) {
+  const updatePosition = function () {
     menuButton.forEach((item, index) => {
       const menuRect = item.getBoundingClientRect();
       submenu[index].style.left = `${menuRect.left}px`;
-      if (topPosition) {
-        submenu[index].style.top = `${135 - topPosition}px`;
-      } else {
-        submenu[index].style.top = `${135}px`;
-      }
+      submenu[index].style.top = `${menuRect.bottom}px`; // Dynamically set the top position based on the button's bottom
     });
   };
+
+  // Call updatePosition immediately after mounting
+  updatePosition();
 
   menuWrapper.addEventListener('scroll', () => {
     updatePosition();
   });
 
   window.addEventListener('scroll', () => {
-    updatePosition(window.pageYOffset);
+    updatePosition();
   });
 
   // Add resize event listener to recalculate positions
   window.addEventListener('resize', () => {
-    updatePosition(window.pageYOffset);
+    updatePosition();
   });
 
   // Cleanup event listeners on unmount
