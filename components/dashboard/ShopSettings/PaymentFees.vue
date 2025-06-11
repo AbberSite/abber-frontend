620aafbf2b2289eb8eb8bbc31161e7800403c89d
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2025-06-11 10:39:40 +0000

    Implemented payment fees management in shop settings, including dynamic inputs for various payment methods and save functionality

--- components/dashboard/ShopSettings/PaymentFees.vue
+++ components/dashboard/ShopSettings/PaymentFees.vue
@@ -1,15 +1,23 @@
 <template>
+    <template v-if="loading"><SkeletonsSocialData/></template>
+    <template v-else>
     <div class="grid grid-cols-1 gap-8 sm:grid-cols-2">
-        <TextInput type="number" label="الخصم على الفيزا وماستركارد:"/>
-        <TextInput type="number" label="الخصم على الفيزا والماستر كارد (ثابت):"/>
-        <TextInput type="number" label="الخصم على مدى:"/>
-        <TextInput type="number" label="الخصم على مدى (ثابت):"/>
-        <TextInput type="number" label="الخصم على stc:"/>
-        <TextInput type="number" label="الخصم على stc (ثابت):"/>
-        <TextInput type="number" label="الخصم على المحفظة:"/>
-        <TextInput type="number" label="الخصم على المحفظة (ثابت):"/>
-        <TextInput type="number" label="الخصم على ابل باي:"/>
-        <TextInput type="number" label="الخصم على ابل باي (ثابت):"/>
+        <TextInput type="number" label="الخصم على الفيزا وماستركارد:" v-model="settings.payment_methods_fees_settings.visa_master_tax"/>
+        <TextInput type="number" label="الخصم على الفيزا والماستر كارد (ثابت):" v-model="settings.payment_methods_fees_settings.fixed_visa_master_tax"/>
+        <TextInput type="number" label="الخصم على مدى:" v-model="settings.payment_methods_fees_settings.mada_tax"/>
+        <TextInput type="number" label="الخصم على مدى (ثابت):" v-model="settings.payment_methods_fees_settings.fixed_mada_tax"/>
+        <TextInput type="number" label="الخصم على stc:" v-model="settings.payment_methods_fees_settings.stc_pay_tax"/>
+        <TextInput type="number" label="الخصم على stc (ثابت):" v-model="settings.payment_methods_fees_settings.fixed_stc_pay_tax"/>
+        <TextInput type="number" label="الخصم على المحفظة:" v-model="settings.payment_methods_fees_settings.balance_tax"/>
+        <TextInput type="number" label="الخصم على المحفظة (ثابت):" v-model="settings.payment_methods_fees_settings.fixed_balance_tax"/>
+        <TextInput type="number" label="الخصم على ابل باي:" v-model="settings.payment_methods_fees_settings.applepay_tax"/>
+        <TextInput type="number" label="الخصم على ابل باي (ثابت):" v-model="settings.payment_methods_fees_settings.fixed_applepay_tax"/>
     </div>
-    <PrimaryButton class="my-2" >حغظ</PrimaryButton>
+    <PrimaryButton class="my-2" :loading="loading" @click="updateSettings(2, {payment_methods_fees_settings: {...settings.payment_methods_fees_settings}})">حفظ</PrimaryButton>
+    </template>
 </template>
+<script setup lang="ts">
+import { useDashboardSettingsStore } from '~/stores/dashboard/dashboardSettings';
+const { settings, loading } = storeToRefs(useDashboardSettingsStore());
+const { updateSettings } = useDashboardSettingsStore();
+</script>
