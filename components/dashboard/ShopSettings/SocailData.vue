16cfba71b5fdba690d48e6437e53e82440edc131
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2025-06-12 13:42:45 +0000

    added ltr for the right inputs

--- components/dashboard/ShopSettings/SocailData.vue
+++ components/dashboard/ShopSettings/SocailData.vue
@@ -4,10 +4,10 @@
     </template>
     <template v-else>
         <div class="grid grid-cols-1 gap-8 sm:grid-cols-2">
-            <TextInput label="فيسبوك:" v-model="settings.social_settings.facebook"/>
-            <TextInput label="سناب شات:" v-model="settings.social_settings.snapchat"/>
-            <TextInput label="تويتر:" v-model="settings.social_settings.twitter"/>
-            <TextInput label="استاغرام:" v-model="settings.social_settings.instagram"/>
+            <TextInput label="فيسبوك:" v-model="settings.social_settings.facebook" dir="ltr"/>
+            <TextInput label="سناب شات:" v-model="settings.social_settings.snapchat" dir="ltr"/>
+            <TextInput label="تويتر:" v-model="settings.social_settings.twitter" dir="ltr"/>
+            <TextInput label="استاغرام:" v-model="settings.social_settings.instagram" dir="ltr"/>
         </div>
         <PrimaryButton class="my-2" :loading="loading" @click="updateSettings(2, {social_settings: {...settings.social_settings}})" >حفظ</PrimaryButton>
     </template>
