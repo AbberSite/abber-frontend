279f0e05e079c4bf11c53f86d00603905e6a1fec
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2025-06-18 16:11:27 +0000

    feat: completed account statements for users in dashboardv2

--- components/dashboard/Expressors/AccountStatement.vue
+++ components/dashboard/Expressors/AccountStatement.vue
@@ -1,26 +1,33 @@
 <template>
-    <!-- <SkeletonsTable v-if="loading"/> -->
     <div class="flex flex-col space-x-3" >
-        <DashboardTablesTable :head-items="head" :body-items="[]" :loading="loading" />
-        <!-- <Pagination
-    class="pt-4"
-    :results="(pagination as PaginationResponse<any>)"
-    @change="getUserLogs"
-    per-page="20"
-  /> -->
+        <DashboardTablesTable :head-items="head" :body-items="accountStatements" :loading="statementsLoading" />
     </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, watchEffect } from 'vue';
+import { useDashboardTransactionsStore } from '~/stores/dashboard/dashboardTransactions';
 import { useDashboardUsersStore } from '~/stores/dashboard/dashboardUsers';
-const { userLogsList, loading, pagination } = storeToRefs(useDashboardUsersStore());
+
+const { userData } = storeToRefs(useDashboardUsersStore());
+const { accountStatements, statementsLoading } = storeToRefs(useDashboardTransactionsStore());
+const { getAccountStatements } = useDashboardTransactionsStore();
 const head = {
-    remote_addr: 'رقم المعاملة',
-    url_path: 'وسيلة الدفع',
-    user_platform: 'نوع المعاملة',
-    ua_string: 'دائن',
-    created_at: 'مدين',
-    "geolocation_for_ip.country_name": 'الرصيد',
-    version: 'تاريخ المعاملة'
+    id: 'رقم المعاملة',
+    amount: 'المبلغ',
+    method: 'وسيلة الدفع',
+    type: 'نوع المعاملة',
+    success: 'عملية ناجحة',
+    date: 'تاريخ المعاملة'
 };
+onMounted(() => {
+    if(!accountStatements.value || accountStatements.value.length === 0) {
+        watchEffect(() => {
+        const username = userData.value?.username;
+            if (username) {
+                getAccountStatements(username);
+            }
+        });
+    }
+});
 </script>
