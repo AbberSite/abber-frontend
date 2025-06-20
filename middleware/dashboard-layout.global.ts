ba7851794d88a04b52452c72e0395a4aba61a45e
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2025-06-20 14:23:39 +0000

    feat(auth): Add authentication check for dashboardv2 routes

--- middleware/dashboard-layout.global.ts
+++ middleware/dashboard-layout.global.ts
@@ -1,6 +1,10 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const {data} = useAuth();
+  
   if (to.path.includes('dashboardv2')) {
+    if (!data.value) {
+      return navigateTo('/accounts/login/');
+    }
     setPageLayout('dashboard');
     if(data.value?.user_type != 'إدارة') return navigateTo('/');
   } 
